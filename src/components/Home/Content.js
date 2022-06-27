6dd881479ec84740a1958a3106e82bc6207cfe68
Spencer Longhurst <spencersean12@gmail.com>
2022-06-27 17:49:18 -0600

    created the jsx for the content card and added some dummy data

--- src/components/Home/Content.js
+++ src/components/Home/Content.js
@@ -5,7 +5,15 @@ import ContentCard from './ContentCard'
 function Content() {
   return (
     <div className='content'>
-      <ContentCard />
+      <ContentCard 
+        image='https://media-exp2.licdn.com/dms/image/C4D0BAQFY3BGhoMwEEA/company-logo_200_200/0/1626195279622?e=1664409600&v=beta&t=aDMV86Socv7bm8G4pRNrGtIkxb2MTfUQp1brjoAXu3s'
+        name='Github'
+        title='2,609,052 followers'
+        timestamp='6d'
+        content='GitHub Copilot helps you get better focus and build faster by instantly suggesting code—and is now available for developers everywhere.
+
+        https://lnkd.in/gAaVpRpi'
+      />
     </div>
   )
 }
--- src/components/Home/ContentCard.js
+++ src/components/Home/ContentCard.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../../styles/ContentCard.css'
+import Avatar from '@mui/material/Avatar';
 
 function ContentCard({ image, name, title, connection, timestamp, content }) {
   return (
     <div className='content_card'>
-
+      <div>
+        <div>
+          <Avatar src={image}/>
+        </div>
+        <div>
+          {name} • {connection}
+          {title}
+          {timestamp}
+        </div>
+      </div>
+      {content}
     </div>
   )
 }
