4a4a628b9ae9cb835b071fdfc7c9ca528cf2a841
Spencer Longhurst <spencersean12@gmail.com>
2022-06-29 16:21:16 -0600

    started the styling for the content card

--- src/components/Home/ContentCard.js
+++ src/components/Home/ContentCard.js
@@ -2,20 +2,24 @@ import React from 'react'
 import '../../styles/ContentCard.css'
 import Avatar from '@mui/material/Avatar';
 
-function ContentCard({ image, name, title, connection, timestamp, content }) {
+function ContentCard({ image, name, title, connection, timestamp, content, contentPhoto }) {
   return (
     <div className='content_card'>
-      <div>
+      <div className='content_info'>
         <div>
-          <Avatar src={image}/>
+          <Avatar className='content_avatar' src={image}/>
         </div>
-        <div>
-          {name} • {connection}
-          {title}
-          {timestamp}
+        <div className='content_details'>
+          <div className='content_intro'>
+            <p className='content_name'>{name}</p>
+            <p className='content_connection'> • {connection}</p>
+          </div>
+          <p className='content_title'>{title}</p>
+          <p className='content_timestamp'>{timestamp}</p>
         </div>
       </div>
-      {content}
+      <p className='content_data'>{content}</p>
+      <img className='content_photo' src={contentPhoto}  alt='contentPhoto'/>
     </div>
   )
 }
