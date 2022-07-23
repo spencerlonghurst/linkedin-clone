28f3627e7d1b7eb90d9c4ba11b5cb21f4853eb8d
Spencer Longhurst <spencersean12@gmail.com>
2022-07-22 23:58:29 -0600

    built out some styling for My Network

--- src/components/My-Network/MyNetwork.js
+++ src/components/My-Network/MyNetwork.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './styles/MyNetwork.css';
 import LeftMyNetwork from './LeftMyNetwork';
+import RightMyNetwork from './RightMyNetwork';
 
 function MyNetwork() {
   return (
     <div className='my_network'>
       <LeftMyNetwork />
+      <RightMyNetwork />
     </div>
   )
 }
---
+++ src/components/My-Network/RightMyNetwork.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/RightMyNetwork.css'
+
+function RightMyNetwork() {
+  return (
+    <div className='right_my_network'>
+      RightMyNetwork
+    </div>
+  )
+}
+
+export default RightMyNetwork
