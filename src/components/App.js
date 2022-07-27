f7cd6b45d1b2a6591dd75dd775afca5f476a8132
Spencer Longhurst <spencersean12@gmail.com>
2022-07-27 16:10:35 -0600

    added dark styling to app for the background color

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,20 @@ import Messaging from './Messaging/Messaging';
 import Jobs from './Jobs/Jobs';
 import MyNetwork from './My-Network/MyNetwork'
 import NavBar from './Nav/NavBar';
+import './App.css'
+import useLocalStorageState from 'use-local-storage-state';
 
 function App() {
+  const [theme, setTheme] = useLocalStorageState('theme' ? 'dark' : 'light')
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme)
+  }
+
   return (
     <Router>
 
-      <div className='main_app'>
+      <div className='main_app'  data-theme={theme}>
           <NavBar />
         <Routes>
           <Route path='/notifications' element={<Notifications />}/>
